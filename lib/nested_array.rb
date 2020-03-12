48207b06f5ec78e68a72d6e7f5a6adbb5b311537
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-12 21:56:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,8 +40,7 @@ def sorted_matrix
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
 
-sorted_matrix[[0][1]].sort
-puts sorted_matrix
+sorted_matrix[[0][1]].sort = sorted_matrix
 
 
 end
