e82222704d51072232abc33a5fb866835e6445b7
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-12 21:53:25 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,7 +40,7 @@ def sorted_matrix
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
 
-sorted_matrix[0].sort!, sorted_matrix[1].sort! = double_sorted_matrix
+sorted_matrix[0].sort, sorted_matrix[1].sort = double_sorted_matrix
 
 double_sorted_matrix
 
