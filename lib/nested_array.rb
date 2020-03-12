9bc1ece89d8f9222b516bb6e3a68774fe6483152
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-12 22:30:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,7 +40,7 @@ def sorted_matrix
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
 
-sorted_matrix.sort! { |a, b| a.min <=> b.min }
+sorted_matrix.sort! { |a, b| a <=> b }
 
 
 
